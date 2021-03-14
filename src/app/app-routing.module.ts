fdd909d597192d65fa1366fb5de473e83b02c0e4
Katlego Morulane <katlego@fusebox.co.za>
2021-03-14 17:47:23 +0200

    restart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '',
+    redirectTo: 'movies',
+    pathMatch: 'full' 
+  },
+  { 
+    path: 'movies',
+    /* 
+    * Differential loading,
+    ? Angular 8 feature 
+    ? Lazy loads movie module
+     */
+    loadChildren: () => import('./components/movie/movie.module').then((m) => m.MovieModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/components/movie/movie-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MovieListComponent } from '../movie-list/movie-list.component';
+
+
+/* 
+* Contains Sub Routes (/) 
+ */
+const routes: Routes = [
+  { 
+    path: '',
+    component: MovieListComponent
+  },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class MovieRoutingModule {}
